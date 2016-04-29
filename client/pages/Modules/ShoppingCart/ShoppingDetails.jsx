f40cf888f3aafd1f7ffd71389ea4c25858d78f26
José Chávez <Jos Chvez>
2016-04-29 18:20:05 -0500

    proceso de compra casi finalizado, falta facturacion

---
+++ client/pages/Modules/ShoppingCart/ShoppingDetails.jsx
@@ -0,0 +1,201 @@
+const {Tabs,Tab,Paper,FloatingActionButton} = mui;
+const {MapsLocalShipping,ActionHistory,ActionPayment} =mui.SvgIcons;
+const {ContentSend} = mui.SvgIcons;
+const Colors = mui.Styles.Colors;
+
+const styles={
+  icons:{
+    margin:'auto',
+    width:28,
+    color:'white',
+    height:28,
+    fill: Colors.pink500
+  },
+  footer:{
+    position:'fixed',
+    display:'flex',
+    justifyContent:'flex-end',
+    left:0,
+    bottom:0,
+    height:60,
+    width:'100%',
+    backgroundColor: Colors.grey100,
+    minWidth: '100%',
+    alignItems:'center'
+  }
+}
+
+ShoppingDetails = React.createClass({
+  contextTypes:{
+    gotUser: React.PropTypes.bool,
+  },
+
+  getInitialState: function() {
+    return {
+      currentTab: '0',
+      validCurrentForm: false,
+      total: 0,
+      noProducts: true,
+      validShippingForm:false,
+      disabledButton:true,
+      loadingButton: false
+    };
+  },
+
+  componentWillMount: function() {
+    this.shipping = {};
+    this.delivery = {};
+    this.token = Session.get('token');
+    Tracker.autorun((a)=>{
+      this.trackerId_a = a;
+      let isShoppingCartEmpty = Session.get('isShoppingCartEmpty');
+      Tracker.nonreactive(()=>{
+        if(isShoppingCartEmpty){
+          this.setState({
+            noProducts: true
+          });
+        }else{
+          this.shoppingCart = Session.get('shoppingCart');
+          this.setState({
+            total: this.shoppingCart.totals[0].text,
+            noProducts: false
+          });
+        }
+      });
+    });
+  },
+
+  handleTabChange:function(value){
+    
+  },
+
+  handleValidShipping:function(idShipping,model){
+    this.setState({
+      validCurrentForm: true,
+      validShippingForm: true,
+      disabledButton:false
+    });
+    this.shipping.model = model;
+    this.shipping.id = idShipping;
+  },
+
+  handleInvalidShipping:function(){
+    this.setState({
+      validCurrentForm: false,
+      validShippingForm: false,
+      disabledButton: true
+    });
+  },
+
+  handleInvalidDelivery:function(){
+    this.setState({
+      disabledButton: true,
+      validDelivery:false,
+      validCurrentForm:false
+    });
+  },
+
+  handleValidDelivery:function(model){
+    this.setState({
+      disabledButton: false,
+      validDelivery:true,
+      validCurrentForm:true
+    });
+    this.delivery.model = model;
+    console.log(model);
+  },
+
+  handleSubmit:function(){
+    const  {gotUser} = this.context;
+    const {validCurrentForm,validDelivery} = this.state;
+    if (gotUser){
+      if(validCurrentForm){
+        switch (this.state.currentTab) {
+          case '0':
+              let {model} = this.shipping;
+              if(model.place_id!=='X')
+              {
+                model.place_id = this.place_id;
+                if(model.selectedAddress === 'NUEVA DIRECCION'){
+                  this.setState({
+                    loadingButton: true
+                  });
+                  formsController.shippingController.addAddress(model,this.token.access_token,
+                    (response)=>{
+                      Session.set('shipping.model',response.data);
+                      Session.set('shipping.id',response.data.address_id);
+                      if (this.isMounted()){
+                        this.setState({
+                          loadingButton: false,
+                          currentTab: '1'
+                        });
+                      }
+                  });
+                }
+                else{
+                  Session.set('shipping.model',model);
+                  Session.set('shipping.id',model.address_id);
+                  this.setState({
+                    loadingButton: false,
+                    currentTab:'1'
+                  });
+                }
+              }
+              break;
+          case '1':
+              model = this.delivery.model;
+              formsController.deliveryController.addDelivery(model,this.token.access_token,
+                (response)=>{
+                  //falta obtener ID
+                });
+            break;
+          case '3':
+
+              break;
+          default:
+        }
+      }
+    }else{
+      this.context.router.push('/login');
+    }
+  },
+
+  render: function() {
+    const {gotUser} = this.context;
+
+    if(gotUser){
+      return (
+        <div>
+          <Tabs value={this.state.currentTab} onChange={this.handleTabChange}>
+            <Tab value = {'0'}
+              icon={<div><MapsLocalShipping style={styles.icons}/></div>}>
+              <ShippingPage invalidShipping = {this.handleInvalidShipping} validShipping={this.handleValidShipping}/>
+            </Tab>
+            <Tab value = {'1'}
+              icon={<div><ActionHistory style={styles.icons}/></div>}>
+              <DeliveryPage invalidDelivery={this.handleInvalidDelivery} validDelivery={this.handleValidDelivery} />
+            </Tab>
+            <Tab value = {'2'}
+              icon={<div><ActionPayment style={styles.icons}/></div>}>
+            </Tab>
+          </Tabs>
+          <div style={styles.footer}>
+            <h3 style={{marginRight:16}} >{'TOTAL: '+ this.state.total}</h3>
+            <FloatingActionButton
+              disabled = {this.state.disabledButton} type="submit" onTouchTap = {this.handleSubmit}>
+              <ContentSend />
+            </FloatingActionButton>
+          </div>
+        </div>
+      );
+    }
+    return(
+      <div>
+        {'Debes estar logueado para comprar algo!'}
+      </div>
+    );
+
+
+  }
+
+});
---
+++ client/pages/ShippingPage.jsx
@@ -0,0 +1,382 @@
+const { TextField,TimePicker, SelectField, DatePicker,RaisedButton,MenuItem,CircularProgress,AutoComplete,FloatingActionButton} = mui;
+const {ContentSend} = mui.SvgIcons;
+
+const Colors = mui.Styles.Colors;
+
+function findById(source, address_id) {
+    return source.filter(function( obj ) {
+        // coerce both obj.id and id to numbers
+        // for val & type comparison
+        return +obj.address_id === +address_id;
+    })[ 0 ];
+}
+
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection:'column',
+    margin: 'auto',
+    justifyContent:'space-around',
+    fontFamily:'Roboto,sans-serif'
+  },
+  field: {
+    margin: 'auto',
+    width:'100%',
+  },
+  form:{
+    display:'flex',
+    flexDirection:'column',
+    alignItems:'center',
+    width:'60%',
+    minWidth:'256',
+    margin:'auto'
+  },
+  headers:{
+    margin:'auto',
+    width:'60%',
+    textAlign:'center',
+    color:Colors.pink500
+  },
+  footer:{
+    position:'fixed',
+    display:'flex',
+    justifyContent:'flex-end',
+    left:0,
+    bottom:0,
+    height:60,
+    width:'100%',
+    backgroundColor: Colors.grey100,
+    minWidth: '100%',
+    alignItems:'center'
+  }
+}
+
+ShippingPage = React.createClass({
+
+  mixins: [React.addons.LinkedStateMixin],
+
+  getDefaultProps: function() {
+    return {
+      footer:true
+    };
+  },
+
+  contextTypes: {
+    screensize: React.PropTypes.string,
+    gotUser: React.PropTypes.bool,
+    router:React.PropTypes.object
+  },
+
+  componentWillMount: function() {
+
+    this.token = Session.get('token');
+    this.setState({
+      addressesLoading: true
+    });
+
+    formsController.getAddresses(this.token.access_token,(err,response)=>{
+      if(response.data.success){
+        this.setState({
+          savedAddress: response.data.data,
+          noAddresses: false,
+          addressesLoading:false
+
+        });
+      }else{
+        this.setState({
+          noAddresses: true,
+          addressesLoading:false
+        });
+      }
+    });
+  },
+
+  errorMessages: {
+    wordsError: "Solo use letras (a-z)",
+    isDefaultRequiredValue: 'Este campo es requerido'
+  },
+
+  handleChangeTimePicker12:function(err, time) {
+    this.refs.picker12hr.setTime(time);
+  },
+
+  componentWillUnmount: function() {
+    Session.set('shippingFormErrors',{});
+  },
+
+  getInitialState: function() {
+    this.placeFinished = 'NOT FINISHED';
+    this.place_id = 'X';
+    return {
+      firstname: '',
+      lastname:'',
+      telephone: '',
+      shippingPlace:'',
+      shippingAddress:'',
+      reference:'',
+      placesDataSource:['Ingresa un lugar'],
+      addressesLoading: true,
+      errors:{},
+      total: 0,
+      errorPlace:'',
+      place:'',
+      validForm:false,
+      selectedAddress: 'NUEVA DIRECCION'
+    };
+  },
+
+  _handleTouchMenuAutoComplete(e){
+    this.place_id = e.currentTarget.id;
+    this.placeFinished = 'FINISHED';
+    formsController.shipping.place_id = this.place_id;
+    this.setState({
+      errorPlace: ''
+    });
+  },
+
+  _handleAutoComplete:function(search){
+
+    this.placeFinished = 'NOT FINISHED';
+    this.place_id = 'X';
+    formsController.shipping.place_id = this.place_id;
+
+    this.setState({
+      place: search
+    });
+    if(search.length>=3){
+      this.setState({
+        placesDataSource: [{
+          text:'Escoge un lugar válido',
+          value: (
+            <MenuItem disabled={true}>
+              <CircularProgress size={0.5} style={{textAlign:'center',width:'100%',margin:0,padding:0}}/>
+            </MenuItem>
+              )
+          }]
+      });
+
+      formsController.getPlaces(search,this.token.access_token,(places)=>{
+        if (places !== 'NO_DATA'){
+          var arrayPlaces = places.map((place)=>{
+            return  {
+                text: place.name,
+                value:
+                  (<MenuItem
+                    primaryText={place.name}
+                    id={place.place_id}
+                    onTouchTap = {this._handleTouchMenuAutoComplete}
+                   />),
+              }
+          });
+          this.setState({
+            placesDataSource: arrayPlaces
+          });
+        }else{
+          this.setState({
+            placesDataSource: [{
+              text:'No encontramos ese lugar',
+              value: (
+                <MenuItem primaryText={'No encontramos ese lugar'} disabled={true}/>
+              )
+              }]
+          });
+        }
+      });
+    }else{
+      this.setState({
+        placesDataSource: ['Ingresa un lugar']
+      });
+    }
+  },
+
+  getAddresses: function(){
+    return this.state.savedAddress.addresses.map((address)=>{
+      return (
+        <MenuItem
+          value = {address.address_id}
+          key = {address.address_id}
+          primaryText={address.address_1}/>
+      );
+    });
+  },
+
+  handleChangeMenuAddresses:function(event, value, index){
+    this.setState({selectedAddress:value});
+    this.shipping = value;
+    formsController.shipping.address_id = value; //GUARDAR ADDRES_ID SELECCIONADO
+    if (value !== 'X'){
+      let address = findById(this.state.savedAddress.addresses,value);
+      console.log(address);
+      this.place_id=address.place_id;
+      this.placeFinished = 'FINISHED';
+      formsController.shipping.isSavedAddress = true;
+      this.setState({
+        disabledForm: true,
+        firstname: address.firstname,
+        lastname: address.lastname,
+        telephone: address.telephone,
+        place: address.place,
+        shippingAddress:address.address_1,
+        reference: address.reference
+      });
+    }else{
+      this.placeFinished = 'NOT FINISHED';
+      formsController.shipping.isSavedAddress = false;  //DETERMINAR SI HUBO CAMBIO
+      this.place_id = 'X';
+      this.setState({
+        disabledForm: false,
+        firstname: '',
+        lastname: '',
+        telephone: '',
+        place: '',
+        shippingAddress: '',
+        reference: ''
+      });
+    }
+  },
+
+  validatePlace:function(event){
+   if(this.place_id === 'X'){
+     this.setState({
+       errorPlace: 'Debe seleccionar un lugar válido'
+     });
+   }else{
+     this.setState({
+       errorPlace: ''
+     });
+   }
+  },
+
+  validateForm:function(){
+    const {validShipping} = this.props;
+    if(this.place_id==='X'){
+      this.setState({
+        validForm: false
+      });
+    }else{
+      model = this.refs.shippingForm.getModel();
+      model.place_id = this.place_id;
+      validShipping(this.shipping,model);
+      this.setState({
+        validForm: true
+      });
+    }
+  },
+
+  invalidForm:function(){
+    this.props.invalidShipping();
+    this.setState({
+      validForm: false
+    });
+  },
+
+  render: function() {
+    let { wordsError } = this.errorMessages;
+    return (
+        <div>
+          <h2 style={styles.headers}>Datos de entrega</h2>
+
+          <Formsy.Form
+            ref={'shippingForm'}
+            onValid={this.validateForm}
+            onInvalid={this.invalidForm}
+            style ={styles.form}>
+
+            <FormsySelect
+              name='selectedAddress'
+              ref='selectedAddress'
+              required
+              onChange={this.handleChangeMenuAddresses}
+              style ={styles.field}
+              floatingLabelText="Elige una dirección"
+              value={this.state.selectedAddress}>
+              <MenuItem value={'X'} primaryText="Nueva dirección"/>
+              {!this.state.addressesLoading?this.getAddresses():<div/>}
+            </FormsySelect>
+
+            <FormsyText
+              name='firstname'
+              ref='firstname'
+              validations='isWords'
+              validationError={wordsError}
+              required
+              floatingLabelText="Nombres"
+              type="string"
+              id ="firstname"
+              value = {this.state.firstname}
+              style ={styles.field}
+              disabled = {this.state.disabledForm}
+            />
+            <FormsyText
+              name='lastname'
+              validations='isWords'
+              validationError={wordsError}
+              required
+              floatingLabelText="Apellidos"
+              type="string"
+              id ="lastname"
+              value = {this.state.lastname}
+              style ={styles.field}
+              disabled = {this.state.disabledForm}
+            />
+            <FormsyText
+              name='telephone'
+              validations='isNumeric'
+              validationError={wordsError}
+              required
+              floatingLabelText="Teléfono"
+              type="number"
+              id ="telephone"
+              style ={styles.field}
+              value = {this.state.telephone}
+              disabled = {this.state.disabledForm}
+            />
+            <AutoComplete floatingLabelText="Lugar"
+              type="string"
+              id ="shippingPlace"
+              searchText = {this.state.place}
+              onBlur = {this.validatePlace}
+              onUpdateInput = {this._handleAutoComplete}
+              openOnFocus = {true}
+              filter={AutoComplete.noFilter}
+              disabled = {this.state.disabledForm}
+              required
+              hintText ={'Coloca un lugar. Ejemplos: Centro, California, etc'}
+              fullWidth={true}
+              style ={styles.field}
+              errorText = {this.state.errorPlace}
+              dataSource = {this.state.placesDataSource}/>
+
+            <FormsyText
+              name='shippingAddress'
+              validationError={wordsError}
+              required
+              floatingLabelText="Dirección de entrega"
+              type="string"
+              id ="shippingAddress"
+              multiLine={true}
+              rows = {2}
+              value = {this.state.shippingAddress}
+              style ={styles.field}
+              disabled = {this.state.disabledForm}
+            />
+            <FormsyText
+              name='reference'
+              validationError={wordsError}
+              required
+              floatingLabelText="Referencia"
+              type="string"
+              id ="reference"
+              multiLine={true}
+              rows={2}
+              value = {this.state.reference}
+              style ={styles.field}
+              disabled = {this.state.disabledForm}
+            />
+          </Formsy.Form>
+        </div>
+    );
+  }
+
+});
