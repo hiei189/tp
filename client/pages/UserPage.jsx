e4a08705bb459bc6650e9fd664e6b91e37d63c1e
Jose <hiei189@gmail.com>
2016-05-05 23:48:43 -0500

    userpage

--- client/pages/UserPage.jsx
+++ client/pages/UserPage.jsx
@@ -8,18 +8,48 @@ const styles={
     color:'white',
     height:28,
     fill: Colors.pink500
+  },
+  container: {
+    display: 'flex',
+    flexDirection:'column',
+    margin: 'auto',
+    justifyContent:'space-around',
+    fontFamily:'Roboto,sans-serif'
+  },
+  field: {
+    margin: 'auto',
+    width:'100%',
+    minWidth:'212',
+  },
+  fieldPrice: {
+    margin: 'auto',
+    width:'calc(100% - 88px)',
+  },
+  form:{
+    margin: 'auto',
+    width:'100%'
+  },
+  headers:{
+    margin:'auto',
+    width:'60%',
+    textAlign:'center',
+    color:Colors.pink500
   }
 }
+
 
 UserPage = React.createClass({
   contextTypes:{
     gotUser: React.PropTypes.bool,
     router:React.PropTypes.object
   },
+
   getInitialState: function() {
     return {
       gotUser: false,
-      user: {}
+      user: {},
+      firstname:'',
+
     };
   },
 
@@ -36,7 +66,6 @@ UserPage = React.createClass({
             gotUser:true
           });
         }else{
-
           this.setState({
             gotUser: false,
             user: {}
@@ -51,11 +80,29 @@ UserPage = React.createClass({
   },
 
   render: function() {
-    const {gotUser} = this.state;
+    const {gotUser,user} = this.state;
+    console.log(user);
     if(gotUser){
       return (
-        <div>
+        <div style={{marginBottom:'50px'}}>
+          <Formsy.Form
+            ref={'userForm'}
+            onValidSubmit={this.submitUserForm}
+            onInvalid = {this.invalidForm}
+            style ={styles.form}>
 
+            <FormsyText
+              required
+              floatingLabelText="Nombres"
+              textFieldStyle = {{width:'100%'}}
+              name = 'userFirstname'
+              id ='userFirstname'
+              value={this.state.firstname}
+              style = {styles.field}
+            />
+
+
+          </Formsy.Form>
         </div>
       );
     }else{
